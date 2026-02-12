e0228f59fe3f3c96750bfa27a7b8185693fc8004
up250176ugla <up250176@alumnos.upa.edu.mx>
2026-02-11 23:55:34 -0600

    Day_1

---
+++ helloworld.py
@@ -0,0 +1,32 @@
+print(3 + 4)
+print(3 - 4)
+print (3 * 4)
+print (3 % 4)
+print(3 / 4)
+print (3 ** 4)
+print (3 // 4)
+
+print ('Luis')
+print ('Uribe')
+print ('Mexico')
+print ('Estoy disfrutando 30 de python')
+
+print (type(10))
+print (type(9.8))
+print (type(3.14))
+print (type(4-4j))
+print (type(['Asabeneh','Python','Finland']))
+print (type('Luis'))   
+print (type('Uribe'))  
+print (type('Mexico'))
+            
+print (type({'numero':'25'}))   
+print (type([1,2,3,4]))     
+print (type(True))  
+print (type({9.8, 4.14, 2.7}))  
+print (type(1 + 3j))
+
+print ('Distancia euclidea')
+x1, y1= 2, 3
+x2, y2= 10, 8
+print((((x2 - x1))**2 + (y2 - y1)**2)**0.5)
