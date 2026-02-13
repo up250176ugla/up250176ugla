e54df040e29c333a4c440aa78d41b73b0fa1123c
up250176ugla <up250176@alumnos.upa.edu.mx>
2026-02-12 22:28:48 -0600

    Day_2

---
+++ variables.py
@@ -0,0 +1,63 @@
+print('Día 2: 30 Days of Python programming')
+first_name = 'Luis'
+last_name = 'Uribe'
+full_name = 'Luis Arturo Uribe González'
+country = 'México'
+city = 'Aguascalientes'
+age = 18
+year = 2026
+is_married = False
+is_true = True
+is_light_on = False
+first_name,city,age = 'Luis', 'Aguascalientes', 18
+
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+
+print(len('first_name'))
+
+print(len(first_name),len(last_name))
+
+num_one = 5
+num_two = 4
+total = num_one + num_two
+print(total)
+diff = num_one - num_two
+print(diff)
+product = num_one * num_two
+print(product)
+division = num_one / num_two
+print(division)
+remainder = num_two % num_one
+print(remainder)
+exp = num_one ** num_two
+print(exp)
+floor_division = num_one // num_two
+print(floor_division)
+
+radius = 30
+area_or_circle = 3.14159 * (radius ** 2)
+print(area_or_circle)
+circum_of_circle = 2 * radius * 3.14159
+print(circum_of_circle)
+user_radius = input("Ingresa el radio del circulo:")
+user_radius = float(user_radius)
+user_area = 3.14159 * (user_radius ** 2)
+print("El area del circulo es:" , user_area)
+
+nombre = input("Ingresa tu nombre:")
+apellido = input("Ingresa tu apellido:")
+pais = input("Ingresa tu pais de origen:")
+edad = input("Ingresa tu edad:")
+print(nombre,apellido,pais,edad)
+
+help('keywords')
+
